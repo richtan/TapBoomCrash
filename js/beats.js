0ad7514e8e8be2f85ab97ff6e88da9ee081641d0
richtan <29845690+richtan@users.noreply.github.com>
2023-09-16 18:04:29 -0400

    Add drum sounds

---
+++ js/beats.js
@@ -0,0 +1,8 @@
+const beats = {
+  tutorial: [
+    'T---',
+    'TT--',
+    'TTTT',
+    'T-t-',
+  ],
+}
--- js/game.js
+++ js/game.js
@@ -4,24 +4,20 @@ class GameScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image('logo_tap', 'assets/tap_neon.png');
-    this.load.image('logo_boom', 'assets/boom_neon.png');
-    this.load.image('logo_crash', 'assets/crash_neon.png');
+    this.load.image('logo_tap', 'assets/logo/tap_neon.png');
+    this.load.image('logo_boom', 'assets/logo/boom_neon.png');
+    this.load.image('logo_crash', 'assets/logo/crash_neon.png');
   }
 
   create() {
     const width = this.scale.gameSize.width;
     const height = this.scale.gameSize.height;
 
-    const tap = this.add.image(150, 200, 'logo_tap');
-    const boom = this.add.image(400, 200, 'logo_boom');
-    const crash = this.add.image(700, 200, 'logo_crash');
+    const tap = this.add.sprite(150, 200, 'logo_tap').setScale(1.25);
+    const boom = this.add.sprite(400, 200, 'logo_boom').setScale(1.25);
+    const crash = this.add.sprite(700, 200, 'logo_crash').setScale(1.25);
 
-    tap.setScale(1.25);
-    boom.setScale(1.25);
-    crash.setScale(1.25);
-
-    const tap_to_start = this.add.text(width/2, 480, 'Tap To Start', {
+    const tap_to_start = this.add.text(width / 2, 480, 'Tap To Start', {
       fontFamily: 'Arial',
       fontSize: '150px',
       fill: '#ffffff',
@@ -39,6 +35,46 @@ class LevelSelectScene extends Phaser.Scene {
   }
 
   create() {
+    this.input.on('pointerdown', () => {
+      this.scene.start('TutorialScene'); // Go to level select when tapped
+    });
+  }
+}
+
+class TutorialScene extends Phaser.Scene {
+  constructor() {
+    super('TutorialScene');
+  }
+
+  preload() {
+    this.load.image('drum', 'assets/tutorial/drum.png');
+    this.load.audio('tutorial_song', 'assets/tutorial/tutorial_song.mp3');
+    this.load.audio('bass_drum_sound', 'assets/tutorial/bass_drum_sound.mp3');
+  }
+
+  create() {
+    const width = this.scale.gameSize.width;
+    const height = this.scale.gameSize.height;
+
+    const drum = this.add.sprite(width / 2, height / 2, 'drum').setOrigin(0.5).setScale(1.0).setInteractive();
+
+    const song = this.sound.add('tutorial_song').setVolume(0.5);
+    const bass = this.sound.add('bass_drum_sound').setVolume(4);
+
+    song.play();
+    this.input.on('pointerdown', () => {
+      bass.play();
+      this.add.timeline([
+        {
+          at: 0,
+          run: () => { drum.setScale(1.1); },
+        },
+        {
+          at: 100,
+          run: () => { drum.setScale(1.0); },
+        },
+      ]).play();
+    });
 
   }
 }
@@ -49,7 +85,7 @@ const config = {
   backgroundColor: '#000000',
   width: 960,
   height: 640,
-  scene: [GameScene, LevelSelectScene],
+  scene: [GameScene, LevelSelectScene, TutorialScene],
 };
 
 const game = new Phaser.Game(config);
