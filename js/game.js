0812eb65e319b08c761d7119630a221bf768cddd
richtan <29845690+richtan@users.noreply.github.com>
2023-09-16 14:10:02 -0400

    Add tap to start

--- js/game.js
+++ js/game.js
@@ -13,13 +13,33 @@ class GameScene extends Phaser.Scene {
     const width = this.scale.gameSize.width;
     const height = this.scale.gameSize.height;
 
-    const tap = this.add.image(200, 200, 'logo_tap');
+    const tap = this.add.image(150, 200, 'logo_tap');
     const boom = this.add.image(400, 200, 'logo_boom');
-    const crash = this.add.image(650, 200, 'logo_crash');
+    const crash = this.add.image(700, 200, 'logo_crash');
 
-    this.scene.launch('GameScene');
+    tap.setScale(1.25);
+    boom.setScale(1.25);
+    crash.setScale(1.25);
 
-    this.gameScene = this.scene.get('GameScene');
+    const tap_to_start = this.add.text(width/2, 480, 'Tap To Start', {
+      fontFamily: 'Arial',
+      fontSize: '150px',
+      fill: '#ffffff',
+    }).setOrigin(0.5);
+
+    this.input.on('pointerdown', () => {
+      this.scene.start('LevelSelectScene'); // Go to level select when tapped
+    });
+  }
+}
+
+class LevelSelectScene extends Phaser.Scene {
+  constructor() {
+    super('LevelSelectScene');
+  }
+
+  create() {
+
   }
 }
 
@@ -29,7 +49,7 @@ const config = {
   backgroundColor: '#000000',
   width: 960,
   height: 640,
-  scene: [GameScene],
+  scene: [GameScene, LevelSelectScene],
 };
 
 const game = new Phaser.Game(config);
