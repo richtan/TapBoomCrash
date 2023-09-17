557231fe1a663c3138a355ba6b0ab0fe95f00edc
hfu314 <140357748+hfu314@users.noreply.github.com>
2023-09-17 02:54:53 -0400

    testing

--- js/LevelSelectScene.js
+++ js/LevelSelectScene.js
@@ -5,6 +5,7 @@ class LevelSelectScene extends Phaser.Scene {
 
   preload() {
     this.load.image('arrow', 'assets/arrow.png');
+    this.load.image('select', 'assets/select_neon.png');
     this.load.image('song1', 'assets/song1.png');
     this.load.image('song2', 'assets/song2.png');
     this.load.image('song3', 'assets/song3.png');
@@ -16,20 +17,11 @@ class LevelSelectScene extends Phaser.Scene {
     const height = this.scale.gameSize.height;
 
     //Arrow
-    const right = this.add.image(width - 60, height / 2, 'arrow');
-    right.setScale(0.1);
-    const left = this.add.image(60, height / 2, 'arrow');
-    left.setScale(0.1);
-    left.setAngle(180);
+    const right = this.add.image(width - 60, height / 2, 'arrow').setScale(0.1);
+    const left = this.add.image(60, height / 2, 'arrow').setScale(0.1).setAngle(180);
 
     //Select
-    const select = this.add.text(width / 2, 540, 'SELECT',
-      {
-        fontFamily: 'Arial',
-        fontSize: '150px',
-        fill: '#ffffff',
-      }).setOrigin(0.5);
-    select.setScale(0.5);
+    const select = this.add.image(width / 2, 600, 'select').setScale(1).setOrigin(0.5);
 
     //Song Detail
     const song = this.add.image(width / 2, height / 2 - 50, 'song1');
