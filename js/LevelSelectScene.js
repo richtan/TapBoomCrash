860ecb6719e3851ffe279ac38ef7483219ee14bc
richtan <29845690+richtan@users.noreply.github.com>
2023-09-17 03:03:48 -0400

    Added new tutorial song and made mobile responsive

--- js/LevelSelectScene.js
+++ js/LevelSelectScene.js
@@ -17,43 +17,54 @@ class LevelSelectScene extends Phaser.Scene {
 
     //Arrow
     const right = this.add.image(width - 60, height / 2, 'arrow');
+    right.setInteractive();
     right.setScale(0.1);
     const left = this.add.image(60, height / 2, 'arrow');
+    left.setInteractive();
     left.setScale(0.1);
     left.setAngle(180);
 
     //Select
-    const select = this.add.text(width / 2, 540, 'SELECT',
+    const select = this.add.text(width / 2, height - 200, 'SELECT',
       {
         fontFamily: 'Arial',
         fontSize: '150px',
         fill: '#ffffff',
       }).setOrigin(0.5);
+    select.setInteractive();
     select.setScale(0.5);
 
     //Song Detail
     const song = this.add.image(width / 2, height / 2 - 50, 'song1');
+    song.setInteractive();
     song.setScale(0.5);
     var song_nm = 1; //current song number
 
-    //Click
+    song.on('pointerdown', () => {
+      this.scene.start('PlayLevelScene', {
+        level_number: song_nm,
+      }); // Go to level select when tapped
+    });
+
+    select.on('pointerdown', () => {
+      this.scene.start('PlayLevelScene', {
+        level_number: song_nm,
+      }); // Go to level select when tapped
+    });
+
     this.input.on('pointerdown', () => {
-      if (this.input.mousePointer.x < width / 3) {
+      if (this.input.mousePointer.x < width / 2) {
         song_nm--;
         if (song_nm <= 0) {
           song_nm = MAX_SONG;
         }
         song.setTexture('song' + song_nm);
-      } else if (this.input.mousePointer.x > width * 2 / 3) {
+      } else {
         song_nm++;
         if (song_nm > MAX_SONG) {
           song_nm = 1;
         }
         song.setTexture('song' + song_nm);
-      } else {
-        this.scene.start('PlayLevelScene', {
-          level_number: song_nm,
-        }); // Go to level select when tapped
       }
     });
   }
--- js/game.js
+++ js/game.js
@@ -7,9 +7,17 @@ const config = {
   type: Phaser.AUTO,
   parent: 'game-canvas',
   backgroundColor: '#000000',
-  width: 960,
-  height: 640,
+  width: window.innerWidth,
+  height: window.innerHeight,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
   scene: [GameScene, LevelSelectScene, TutorialScene, PlayLevelScene],
 };
 
 const game = new Phaser.Game(config);
+
+window.addEventListener('resize', () => {
+  game.scale.resize(window.innerWidth, window.innerHeight);
+});
